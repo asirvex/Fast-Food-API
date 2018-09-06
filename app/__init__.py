660436e3b87b4dc5ae41ab1d39ff1eca667915b9
Constantine <asirvex@gmail.com>
2018-09-06 17:31:14 +0300

    adds the post order functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,22 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request
 from instance.config import app_config
 
-
-orders = [{
+foods =  [{
     "id": 1,
-    "food": "burger",
+    "name": "burger",
     "price": 80
 }, {
     "id": 2,
-    "food": "smokey",
+    "name": "smokey",
     "price": 35
+}, {
+    "id": 3,
+    "name": "chicken sandwich",
+    "price": 150
 }
 ]
+
+orders = []
 
 def create_App(config_name):
     """app configuration"""
@@ -30,6 +35,13 @@ def create_App(config_name):
             if order["id"] == orderId:
                 return jsonify(order)
 
+    @app.route("/orders", methods=["POST"])
+    def post_orders():
+        order_input = request.json["name"],
+        for food in foods:
+            if order_input[0] == food["name"]:
+                orders.append(food)
+                return jsonify(orders)
+        return "food not found"
 
     return app
-
