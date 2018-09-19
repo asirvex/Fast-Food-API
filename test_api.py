49072ab4e5e294b8694f1917e5d77fc8ccd7b798
Constantine <asirvex@gmail.com>
2018-09-19 10:25:45 +0300

    fixed tests

--- test_api.py
+++ test_api.py
@@ -1,14 +1,14 @@
 import unittest
 import os
 import json
-from app import create_App
+from app import create_app
 
 class FastFoodTestCase(unittest.TestCase):
     """This class tests the endpoints"""
     
     def setUp(self):
         """initialise app and test variables"""
-        self.app = create_App(config_name="testing")
+        self.app = create_app(config_name="testing")
         self.client = self.app.test_client
         self.EntryData = json.dumps({
                                     "name": "burger"
@@ -27,7 +27,7 @@ class FastFoodTestCase(unittest.TestCase):
     def test_post_orders(self):
         """test if an order is posted successfully"""
         res = self.client().post("/api/v1/orders", data=self.EntryData, content_type = "application/json")
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 201)
 
     def test_edit_orders(self):
         """check that an order is editted succeffully"""
